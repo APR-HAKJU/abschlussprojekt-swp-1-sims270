3e1bfca7f44c14a2c8a9ac19baa74c0e4df10a38
tonytonychopper96 <leon.wedam@gmail.com>
2026-01-30 08:18:48 +0100

    maybe_done

--- Assets/Ilumisoft/Minesweeping/Scripts/GameManager.cs
+++ Assets/Ilumisoft/Minesweeping/Scripts/GameManager.cs
@@ -22,7 +22,9 @@ namespace Ilumisoft.Minesweeping
         TileGrid grid = null;
 
         [SerializeField]
-        int bombCount = 5;
+        private int bombCount = 5;
+
+        public int BombCount { get => bombCount; }
 
         GameObject tileContainer = null;
 
@@ -35,6 +37,7 @@ namespace Ilumisoft.Minesweeping
 
         void Start()
         {
+            Debug.Log($"[GameManager] Starting with bombCount={bombCount}, grid={grid.Width}x{grid.Height}");
             CreateBoard();
         }
 
@@ -56,14 +59,38 @@ namespace Ilumisoft.Minesweeping
             // Make sure the number of bombs is not larger than the grid size
             bombCount = Mathf.Min(bombCount, grid.Width * grid.Height);
 
-            // Place bombs in the 4 rightmost tiles of the top row
-            for (int i = 1; i < bombCount && i <= 4; i++)
+            // Calculate random positions for the bombs
+            int placed = 0;
+
+            // Place bombs at random unique positions until we've placed bombCount bombs
+            while (placed < bombCount)
             {
-                int x = grid.Width - i;
-                AddTileToGrid(x, 0, bombTilePrefab);
+                int x = Random.Range(0, grid.Width);
+                int y = Random.Range(0, grid.Height);
+
+                // If there is already a tile at this position, skip it
+                if (grid.TryGetTile(x, y, out _))
+                {
+                    continue;
+                }
+
+                AddTileToGrid(x, y, bombTilePrefab);
+                placed++;
             }
+
+            Debug.Log($"[GameManager] Placed {placed} bombs (requested {bombCount})");
         }
 
+
+        /// <summary>
+        /// Allows changing the bomb count at runtime before board creation.
+        /// Call this before the scene starts or before `CreateBoard()` is invoked.
+        /// </summary>
+        /// <param name="count"></param>
+        public void SetBombCount(int count)
+        {
+            bombCount = Mathf.Max(0, count);
+        }
         /// <summary>
         /// Fills all empty grid cells with normal tiles
         /// </summary>
